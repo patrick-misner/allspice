eb40540d1407c3a14dfd8aa5efc7c54fab62a575
Patrick Misner <misner.patrick@gmail.com>
2022-07-21 15:21:35 -0600

    test

--- allspice/Models/Ingredient.cs
+++ allspice/Models/Ingredient.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace allspice.Models
 {
-    public class Ingredient
-    {
-        
-    }
+  public class Ingredient
+  {
+
+
+  }
 }
+
--- allspice/Models/Recipe.cs
+++ allspice/Models/Recipe.cs
@@ -2,6 +2,12 @@ namespace allspice.Models
 {
   public class Recipe
   {
-
+    public int Id { get; set; }
+    public string Picture { get; set; } = "https://www.thiscatdoesnotexist";
+    public string Title { get; set; }
+    public string Subtitle { get; set; }
+    public string Category { get; set; }
+    public string CreatorId { get; set; }
+    public Profile Creator { get; set; }
   }
 }
