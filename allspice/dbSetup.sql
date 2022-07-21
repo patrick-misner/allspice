8420f1abfcd05ed539b758473c250b90a580256a
Patrick Misner <misner.patrick@gmail.com>
2022-07-21 15:02:42 -0600

    test

--- allspice/dbSetup.sql
+++ allspice/dbSetup.sql
@@ -7,3 +7,50 @@ CREATE TABLE
         email varchar(255) COMMENT 'User Email',
         picture varchar(255) COMMENT 'User Picture'
     ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS recipes(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        picture TEXT NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        subtitle VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8;
+
+CREATE TABLE
+    IF NOT EXISTS ingredients(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name VARCHAR(255) NOT NULL,
+        quantity VARCHAR(255) NOT NULL,
+        recipeId INT NOT NULL,
+        FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+    ) default charset utf8;
+
+CREATE TABLE
+    IF NOT EXISTS steps(
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        position INT NOT NULL,
+        body TEXT NOT NULL,
+        recipeId INT NOT NULL,
+        FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
+    ) default charset utf8;
+
+/* 
+ CREATE TABLE
+ IF NOT EXISTS favorites(
+ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+ updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+ recipeId INT NOT NULL,
+ FOREIGN KEY (recipeId) REFERENCES recipes(id),
+ accountId VARCHAR(255) NOT NULL,
+ FOREIGN KEY (accountId) REFERENCES account(id) ON DELETE CASCADE
+ ) default charset utf8; */
