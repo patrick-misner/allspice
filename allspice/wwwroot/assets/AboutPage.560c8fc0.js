5b482cb69634cce9e22108243e3116b51d67a155
Patrick Misner <misner.patrick@gmail.com>
2022-09-06 04:28:57 -0600

    updates

---
+++ allspice/wwwroot/assets/AboutPage.560c8fc0.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.93ad36a7.js";import{o,b as t,d as s}from"./vendor.89128711.js";const a={name:"AboutPage"},n={class:"about"},r=s("h1",null,"This is the about page",-1),c=[r];function _(d,i,p,u,f,h){return o(),t("div",n,c)}var b=e(a,[["render",_]]);export{b as default};
